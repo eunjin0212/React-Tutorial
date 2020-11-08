5b88409fce3ca3414c931d80d761ac2448e8050d
eunjin0212 <eunjin9639@gmail.com>
2020-11-08 17:35:58 +0900

    ...Spread Operator

--- ReactBase.js
+++ ReactBase.js
@@ -1,22 +1,5 @@
-const human = {
-  name: "eunjin",
-  lastName: "Kim",
-  nationality: "Wish i was Korean",
-  favFood: {
-    breakfast: "Sang",
-    lunch: "donc",
-    dinner: "Sang" + "donc",
-  },
-};
-// const name = human.name;
-// const lastName = human.lastName;
-// const difNmae = human.nationality;
-//const dinner = human.favFood.dinner;
+const days = ["Mon", "Tues", "Wed"];
+const otherDays = ["Thu", "Fri", "Sat"];
+const allDays = [...days, ...otherDays];
 
-const {
-  name,
-  lastName,
-  nationality: difName,
-  favFood: { dinner, breakfast, lunch },
-} = human;
-console.log(name, lastName, difName, dinner, breakfast, lunch);
+console.log(allDays);
