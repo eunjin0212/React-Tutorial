1afa60873bc023b5f54660758129aa2a1a54f477
eunjin0212 <eunjin9639@gmail.com>
2020-11-08 16:08:06 +0900

    ArrowFunc

---
+++ ArrowFunc.js
@@ -0,0 +1,12 @@
+function sayHello(name) {
+  return "hello " + name;
+}
+
+//Arrow Function
+const sayHello2 = (name) => "hello " + name;
+
+const eunjin = sayHello("eunjin");
+const eunjin2 = sayHello2("eunjin2");
+
+console.log(eunjin);
+console.log(eunjin2);
