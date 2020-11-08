1cf5985cae86808d086d5288085d205bd3210f71
eunjin0212 <eunjin9639@gmail.com>
2020-11-08 16:20:48 +0900

    Parameter

--- ArrowFunc.js
+++ ArrowFunc.js
@@ -1,12 +1,12 @@
-function sayHello(name) {
+function sayHello(name = "eunjin") {
   return "hello " + name;
 }
 
 //Arrow Function
-const sayHello2 = (name) => "hello " + name;
+const sayHello2 = (name = "eunjin!") => "hello " + name;
 
-const eunjin = sayHello("eunjin");
-const eunjin2 = sayHello2("eunjin2");
+const eunjin = sayHello();
+const eunjin2 = sayHello2();
 
 console.log(eunjin);
 console.log(eunjin2);
