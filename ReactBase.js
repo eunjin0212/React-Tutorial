9ed8157ab7b1ab44382217ae5d2af838b15a6bef
eunjin0212 <eunjin9639@gmail.com>
2020-11-08 16:46:26 +0900

    Object Destructuring

--- ReactBase.js
+++ ReactBase.js
@@ -2,7 +2,21 @@ const human = {
   name: "eunjin",
   lastName: "Kim",
   nationality: "Wish i was Korean",
+  favFood: {
+    breakfast: "Sang",
+    lunch: "donc",
+    dinner: "Sang" + "donc",
+  },
 };
+// const name = human.name;
+// const lastName = human.lastName;
+// const difNmae = human.nationality;
+//const dinner = human.favFood.dinner;
 
-const { name, lastName } = human;
-console.log(name, lastName);
+const {
+  name,
+  lastName,
+  nationality: difName,
+  favFood: { dinner, breakfast, lunch },
+} = human;
+console.log(name, lastName, difName, dinner, breakfast, lunch);
