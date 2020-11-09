35c5a5148731cee20b04a92db626d719144f5d8f
eunjin0212 <eunjin9639@gmail.com>
2020-11-09 13:42:17 +0900

    map

--- ReactBase.js
+++ ReactBase.js
@@ -1,19 +1,4 @@
-class Human {
-  constructor(name, lastName) {
-    this.name = name;
-    this.lastName = lastName;
-  }
-}
-const eunjin = new Human("eunjin", "kim");
-console.log(eunjin.name);
+const days = ["Mon", "Tue", "Wed", "Thurs", "Fri"];
 
-class Baby extends Human {
-  cry() {
-    console.log("Waaaaa");
-  }
-  sayName() {
-    console.log(`My name is ${this.name}`);
-  }
-}
-const myBaby = new Baby("mini", "me");
-console.log(myBaby.cry(), myBaby.sayName());
+const smilingDays = days.map((day, index) => `#${index + 1} ${day}`);
+console.log(smilingDays);
