8adec9fd1c6ab2a72275f2514230947a710cd6b8
eunjin0212 <eunjin9639@gmail.com>
2020-11-09 14:12:57 +0900

    forEach

--- ReactBase.js
+++ ReactBase.js
@@ -1,10 +1,2 @@
-const numbers = [2, 45, 6564, 33, 345, 45, 3, 6, 12, 112, 46, 78, 8, 98, 8];
-const testCondition = (number) => number < 15;
-//const biggerThan15 = numbers.filter((number) => number > 15);
-const smallThan15 = numbers.filter(testCondition);
-//console.log(biggerThan15);
-console.log(smallThan15);
-
 let posts = ["hi", "hello", "bye"];
-posts = posts.filter((post) => post !== "bye");
-console.log(posts);
+posts.forEach((post) => console.log(post));
