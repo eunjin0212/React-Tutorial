8a6b37f470d6344d1f428f547b736a06861eb322
eunjin0212 <eunjin9639@gmail.com>
2020-11-09 14:00:39 +0900

    filter

--- ReactBase.js
+++ ReactBase.js
@@ -1,4 +1,10 @@
-const days = ["Mon", "Tue", "Wed", "Thurs", "Fri"];
+const numbers = [2, 45, 6564, 33, 345, 45, 3, 6, 12, 112, 46, 78, 8, 98, 8];
+const testCondition = (number) => number < 15;
+//const biggerThan15 = numbers.filter((number) => number > 15);
+const smallThan15 = numbers.filter(testCondition);
+//console.log(biggerThan15);
+console.log(smallThan15);
 
-const smilingDays = days.map((day, index) => `#${index + 1} ${day}`);
-console.log(smilingDays);
+let posts = ["hi", "hello", "bye"];
+posts = posts.filter((post) => post !== "bye");
+console.log(posts);
